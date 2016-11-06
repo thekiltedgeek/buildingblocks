513ead046d0e62891d4a508a80b6150535c86fa3
Larry Shanahan <thekiltedgeek@gmail.com>
2016-11-06 16:48:52 -0500

    passed all tests. updated README

--- StockPicker/stock_picker.rb
+++ StockPicker/stock_picker.rb
@@ -4,4 +4,17 @@
 #return a pair of days representing the best days to buy and sell.
 
 def stock_picker(prices)
+    result = [0,[]]
+    for i in 0..prices.length - 2 do
+        for j in i + 1..prices.length - 1 do
+            diff = prices[j] - prices[i]
+            if diff > result[0]
+                result[0] = diff
+                result[1] = [i,j]
+            end
+        end
+    end
+    
+    #return result[0] > 0 ? result[1] : "None"
+    return result[1]
 end
--- StockPicker/stock_picker_rspec.rb
+++ StockPicker/stock_picker_rspec.rb
@@ -3,8 +3,16 @@
 require_relative 'stock_picker'
 
 describe "stock_picker" do
-    @prices = [17,3,6,9,15,8,6,1,10]
-    it "should return two numbers" do
-        expect(stock_picker(@prices)).to eq([1,4])
+    it "[17,3,6,9,15,8,6,1,10] should return [1,4]" do
+        expect(stock_picker([17,3,6,9,15,8,6,1,10])).to eq([1,4])
+    end
+    it "[1,2,3,4,5] should return [1,5]" do
+        expect(stock_picker([1,2,3,4,5])).to eq([0,4])
+    end
+    it "[5,1,2,3,4] should return [2,5]" do
+        expect(stock_picker([5,1,2,3,4])).to eq([1,4])
+    end
+    it "[5,4,3,2,1] should return empty array" do
+        expect(stock_picker([5,4,3,2,1])).to eq([])
     end
 end
