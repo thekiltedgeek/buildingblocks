a4add3abcc0298104dfcbc591102737a5f5d0901
Larry Shanahan <thekiltedgeek@gmail.com>
2016-11-06 17:30:08 -0500

    created (empty) substrings method, set up rspec tests

---
+++ Substrings/substrings.rb
@@ -0,0 +1,6 @@
+#substrings.rb
+#
+#Given a dictionary array and a source string containing one or more words, return a histogram showing how many times each word in the dictionary occurs as a case-insensitive substring of the source string.
+
+def substrings(phrase, dictionary)
+end
